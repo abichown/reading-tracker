61d962ad50d5a2c3df67715563136a83181c01f7
Abi Chown <abi.chown7@gmail.com>
2026-05-27 14:26:13 +0100

    Refactor search bar

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,24 +1,16 @@
-import { useEffect, useState } from "react";
-
 interface SearchBarProps {
     onSearch: (searchTerm: string) => void;
 }
 
 const SearchBar = ({ onSearch }: SearchBarProps) => {
-    const [searchTerm, setSearchTerm] = useState("");
-
-    const handleUserInput = (event: any) => {
-        setSearchTerm(event.target.value);
-    };
-
-    useEffect(() => {
-        onSearch(searchTerm);
-    }, [searchTerm]);
-
     return (
         <div>
-            <label id="search">Search bar</label>
-            <input name="search" onChange={handleUserInput} />
+            <label htmlFor="search">Search bar</label>
+            <input
+                id="search"
+                name="search"
+                onChange={(event) => onSearch(event.target.value)}
+            />
         </div>
     );
 };
